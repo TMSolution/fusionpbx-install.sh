cd5b11c4e20444ee6d3c801684c44c13bf5f25d8
mariuszpiela <mariusz.piela@tmsolution.pl>
2024-02-01 12:51:41 +0100

    Update install2.sh

--- debian/install2.sh
+++ debian/install2.sh
@@ -1,1 +1,36 @@
+#!/bin/sh
 
+#move to script directory so all relative paths work
+cd "$(dirname "$0")"
+
+#includes
+. ./resources/config.sh
+. ./resources/colors.sh
+. ./resources/environment.sh
+
+#sngrep
+resources/sngrep.sh
+
+#FusionPBX
+resources/fusionpbx.sh
+
+#PHP
+resources/php.sh
+
+#NGINX web server
+resources/nginx.sh
+
+#FreeSWITCH
+resources/switch.sh
+
+#Fail2ban
+resources/fail2ban.sh
+
+#Postgres
+resources/postgresql.sh
+
+#set the ip address
+server_address=$(hostname -I)
+
+#add the database schema, user and groups
+resources/finish.sh
